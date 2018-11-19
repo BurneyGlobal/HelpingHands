f953170b2d9a7a1feaf87b91ce5e1705bd1503d7
irene jona smith irenejonasmith12@gmail.com <irenesmith@gmx.com>
2018-11-19 17:19:09 +0100

    added events migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_154717) do
+ActiveRecord::Schema.define(version: 2018_11_19_161648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "location"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
