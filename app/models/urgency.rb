c4046cb14d367470361560e38d147c1cee11a17a
Jerrel Burney <burneyglobalstrategies@gmail.com>
2018-11-19 17:17:04 +0100

    created urgencies and professions model

---
+++ app/models/urgency.rb
@@ -0,0 +1,2 @@
+class Urgency < ApplicationRecord
+end
---
+++ test/models/urgency_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UrgencyTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
