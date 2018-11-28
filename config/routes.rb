aea0af77a771a99ae23c9dd2b6af06f19ce7da43
Anna Litfin <litfin_k@yahoo.de>
2018-11-28 16:45:10 +0100

    task-show-page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
     resources :resource_assets, only: [ :index ]
   end
 
+  post "tasks/:task_id/users/:user_id/assign", to: 'task_volunteers#create', as: :assign_task
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
