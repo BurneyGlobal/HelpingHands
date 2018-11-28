0153fffa558da72ab7cfeb1a4aaac0dce612ba85
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-11-28 12:12:09 +0100

    Add link on the copenhagen card that links to the event

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @events = Event.all.select(&:location)
-
-    @markers = @events.map do |event|
-      {
-        lng: event.location.longitude,
-        lat: event.location.latitude
-      }
-    end
+    @events = Event.where(name: "Flood in Copenhagen")
   end
 end
