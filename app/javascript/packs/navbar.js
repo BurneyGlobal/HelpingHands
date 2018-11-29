014ece37365e29a3314725767a63ddbd3563c7cf
Henri Stenberg <henri.stenberg@gmail.com>
2018-11-29 15:27:24 +0100

    navbar links done

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -21,28 +21,28 @@
 
 // <<<<<<<<<<<<<<<<<<
 
-(function($) {
-    "use strict";
+// (function($) {
+//     "use strict";
 
-    var $navbar = $("#navbar"),
-        y_pos = $navbar.offset().top,
-        height = $navbar.height();
+//     var $navbar = $("#navbar"),
+//         y_pos = $navbar.offset().top,
+//         height = $navbar.height();
 
-    $(document).scroll(function() {
-        var scrollTop = $(this).scrollTop();
+//     $(document).scroll(function() {
+//         var scrollTop = $(this).scrollTop();
 
-        if (scrollTop > y_pos + height) {
-            $navbar.addClass("navbar-fixed").animate({
-                top: 0
-            });
-        } else if (scrollTop <= y_pos) {
-            $navbar.removeClass("navbar-fixed").clearQueue().animate({
-                top: "-48px"
-            }, 0);
-        }
-    });
+//         if (scrollTop > y_pos + height) {
+//             $navbar.addClass("navbar-fixed").animate({
+//                 top: 0
+//             });
+//         } else if (scrollTop <= y_pos) {
+//             $navbar.removeClass("navbar-fixed").clearQueue().animate({
+//                 top: "-48px"
+//             }, 0);
+//         }
+//     });
 
-})(jQuery, undefined);
+// })(jQuery, undefined);
 
 
 
